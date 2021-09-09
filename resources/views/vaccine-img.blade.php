7bef2654d9d3cca45615098233ef101637d950de
mehdi asgari <it1@abharh.ir>
2021-09-09 12:01:09 +0430

    vaccine-img3

--- resources/views/vaccine-img.blade.php
+++ resources/views/vaccine-img.blade.php
@@ -6,7 +6,7 @@
         <div class="row justify-content-center">
 
             <div class="col-md-12 mt-2">
-<h1> پوشش واکسیناسیون تا 13 شهریور</h1>
+<h1> پوشش واکسیناسیون تا 17 شهریور</h1>
                 <ul class="nav nav-pills">
                     <li  class="btn btn-default"><a data-toggle="pill" href="#home">زنجان</a></li>
                     <li class="btn btn-default"><a data-toggle="pill" href="#menu1">ابهر</a></li>
@@ -21,39 +21,140 @@
                 <div class="tab-content">
                     <div id="home" class="tab-pane fade in active">
                         <h3>زنجان</h3>
-                        <img src="/images/vaccine/zanjan1.jpg" width="100%"/>
-                        <img src="/images/vaccine/zanjan2.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 40 سال</h4>
+                        <img src="/images/vaccine/zanjan40-1.jpg" width="100%"/>
+                        <img src="/images/vaccine/zanjan40-2.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 50 سال</h4>
+                        <img src="/images/vaccine/zanjan50-1.jpg" width="100%"/>
+                        <img src="/images/vaccine/zanjan50-2.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 60 سال</h4>
+                        <img src="/images/vaccine/zanjan60-1.jpg" width="100%"/>
+                        <img src="/images/vaccine/zanjan60-2.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 70 سال</h4>
+                        <img src="/images/vaccine/zanjan70-1.jpg" width="100%"/>
+                        <img src="/images/vaccine/zanjan70-2.jpg" width="100%"/>
+
                     </div>
                     <div id="menu1" class="tab-pane fade">
                         <h3>ابهر</h3>
-                        <img src="/images/vaccine/abhar1.jpg" width="100%"/>
-                        <img src="/images/vaccine/abhar2.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 40 سال</h4>
+                        <img src="/images/vaccine/abhar40-1.jpg" width="100%"/>
+                        <img src="/images/vaccine/abhar40-2.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 50 سال</h4>
+                        <img src="/images/vaccine/abhar50-1.jpg" width="100%"/>
+                        <img src="/images/vaccine/abhar50-2.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 60 سال</h4>
+                        <img src="/images/vaccine/abhar60-1.jpg" width="100%"/>
+                        <img src="/images/vaccine/abhar60-2.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 70 سال</h4>
+                        <img src="/images/vaccine/abhar70-1.jpg" width="100%"/>
+                        <img src="/images/vaccine/abhar70-2.jpg" width="100%"/>
                     </div>
                     <div id="menu2" class="tab-pane fade">
                         <h3>خدابنده</h3>
-                        <img src="/images/vaccine/khodabande1.jpg" width="100%"/>
-                        <img src="/images/vaccine/khodabande2.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 40 سال</h4>
+                        <img src="/images/vaccine/khodabande40-1.jpg" width="100%"/>
+                        <img src="/images/vaccine/khodabande40-2.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 50 سال</h4>
+                        <img src="/images/vaccine/khodabande50-1.jpg" width="100%"/>
+                        <img src="/images/vaccine/khodabande50-2.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 60 سال</h4>
+                        <img src="/images/vaccine/khodabande60-1.jpg" width="100%"/>
+                        <img src="/images/vaccine/khodabande60-2.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 70 سال</h4>
+                        <img src="/images/vaccine/khodabande70-1.jpg" width="100%"/>
+                        <img src="/images/vaccine/khodabande70-2.jpg" width="100%"/>
                     </div>
                     <div id="menu3" class="tab-pane fade">
                         <h3>خرمدره</h3>
-                        <img src="/images/vaccine/khoramdare1.jpg" width="100%"/>
-                        <img src="/images/vaccine/khoramdare2.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 40 سال</h4>
+                        <img src="/images/vaccine/khoramdare40-1.jpg" width="100%"/>
+                        <img src="/images/vaccine/khoramdare40-2.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 50 سال</h4>
+                        <img src="/images/vaccine/khoramdare50-1.jpg" width="100%"/>
+                        <img src="/images/vaccine/khoramdare50-2.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 60 سال</h4>
+                        <img src="/images/vaccine/khoramdare60-1.jpg" width="100%"/>
+                        <img src="/images/vaccine/khoramdare60-2.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 70 سال</h4>
+                        <img src="/images/vaccine/khoramdare70-1.jpg" width="100%"/>
+                        <img src="/images/vaccine/khoramdare70-2.jpg" width="100%"/>
                     </div>
                     <div id="menu4" class="tab-pane fade">
                         <h3>ایجرود</h3>
-                        <img src="/images/vaccine/ijrood1.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 40 سال</h4>
+                        <img src="/images/vaccine/ijrood40-1.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 50 سال</h4>
+                        <img src="/images/vaccine/ijrood50-1.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 60 سال</h4>
+                        <img src="/images/vaccine/ijrood60-1.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 70 سال</h4>
+                        <img src="/images/vaccine/ijrood70-1.jpg" width="100%"/>
                     </div>
                     <div id="menu5" class="tab-pane fade">
                         <h3>ماهنشان</h3>
-                        <img src="/images/vaccine/mahneshan1.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 40 سال</h4>
+                        <img src="/images/vaccine/mahneshan40-1.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 50 سال</h4>
+                        <img src="/images/vaccine/mahneshan50-1.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 60 سال</h4>
+                        <img src="/images/vaccine/mahneshan60-1.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 70 سال</h4>
+                        <img src="/images/vaccine/mahneshan70-1.jpg" width="100%"/>
                     </div>
                     <div id="menu6" class="tab-pane fade">
                         <h3>سلطانیه</h3>
-                        <img src="/images/vaccine/soltanie1.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 40 سال</h4>
+                        <img src="/images/vaccine/soltanie40-1.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 50 سال</h4>
+                        <img src="/images/vaccine/soltanie50-1.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 60 سال</h4>
+                        <img src="/images/vaccine/soltanie60-1.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 70 سال</h4>
+                        <img src="/images/vaccine/soltanie70-1.jpg" width="100%"/>
                     </div>
                     <div id="menu7" class="tab-pane fade">
                         <h3>طارم</h3>
-                        <img src="/images/vaccine/tarom1.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 40 سال</h4>
+                        <img src="/images/vaccine/tarom40-1.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 50 سال</h4>
+                        <img src="/images/vaccine/tarom50-1.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 60 سال</h4>
+                        <img src="/images/vaccine/tarom60-1.jpg" width="100%"/>
+                        <hr/>
+                        <h4>رنگبندی پوشش واکسیناسیون بالای 70 سال</h4>
+                        <img src="/images/vaccine/tarom70-1.jpg" width="100%"/>
                     </div>
                 </div>
             </div>
